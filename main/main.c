0866977119f8f3c9825e925e2c9bf091e4e1fb36
Ratextak <estrefugaz12@gmail.com>
2023-01-23 19:45:09 +0100

    Abrir/cerrar bombas funcional

--- main/main.c
+++ main/main.c
@@ -17,7 +17,7 @@
 #define BOMBA_K_PIN         GPIO_NUM_27  // Potasio.
 #define VALVULA_PIN_1       GPIO_NUM_18  // Válvula solenoide (2 pines).
 #define VALVULA_PIN_2       GPIO_NUM_19
-#define TEMP_SENSOR_PIN     GPIO_NUM_22  // Sonda de temperatura.
+#define TEMP_SENSOR_PIN     ADC1_CHANNEL_7//GPIO_NUM_22  // Sonda de temperatura.
 #define NIVEL_SENSOR_PIN    GPIO_NUM_23  // Sensor de nivel de líquidos.
 #define ELECTRO_SENSOR_PIN  ADC1_CHANNEL_6  // Sensor de electroconductividad (GPIO 34).
  
@@ -33,19 +33,21 @@ static esp_adc_cal_characteristics_t adc1_chars;
 float tempValor = 25.0, ecValor, ecVoltaje, ecAux;
 bool nivelValor;
 
-enum estado {CERRADA , ABIERTA};  // Estado para las bombas y la válvula.
-enum estado valvula = CERRADA;  // Empiezan cerradas.
-enum estado bombaN = CERRADA, bombaP = CERRADA, bombaK = CERRADA;
+enum estadoBombValv {CERRADA , ABIERTA};  // Estado para las bombas y la válvula.
+enum estadoBombValv valvula = CERRADA;  // Empiezan cerradas.
+enum estadoBombValv bombaN = CERRADA, bombaP = CERRADA, bombaK = CERRADA;
 
-enum estado1 {MEDIR, DISPENSAR, VACIAR, TRANSMITIR, RECIBIR};
-enum estado1 modo = MEDIR;  // Cotrolará el modo en el que trabaja el sistema.
+enum estadoSist {MEDIR, DISPENSAR, VACIAR, TRANSMITIR, RECIBIR};
+enum estadoSist modo = MEDIR;  // Cotrolará el modo en el que trabaja el sistema.
 
-enum estado2 {VACIO, LLENO};
-enum estado2 tanque = VACIO;  // Estado del tanque dónde se dispensarán los nutrientes.
+enum estadoTanque {VACIO, LLENO};
+enum estadoTanque tanque = VACIO;  // Estado del tanque dónde se dispensarán los nutrientes.
 
 
 void medir_temperatura(void) {
-    tempValor = ds18b20_get_temp();  // Obtenemos la temperatura del sensor.
+    //tempValor = ds18b20_get_temp();  // Obtenemos la temperatura del sensor.
+    float tempVoltaje = esp_adc_cal_raw_to_voltage(adc1_get_raw(TEMP_SENSOR_PIN), &adc1_chars);
+    tempValor = ((tempVoltaje-142)/(3002/56)) - 6;
     printf("Temperatura: %0.1f ºC\n", tempValor);
 }
 
@@ -68,34 +70,60 @@ void medir_electroconductividad(void) {
 }
 
 void control_Bombas_Valvula(void) {
+    enum estadoBombValv valvula_ant = ABIERTA;  // Controlan si cambian de estado.
+    enum estadoBombValv bombaN_ant = ABIERTA, bombaP_ant = ABIERTA, bombaK_ant = ABIERTA;
+    
     while (1) {
-        if (valvula == ABIERTA) {
-            gpio_set_level(VALVULA_PIN_1, 1);
-            gpio_set_level(VALVULA_PIN_2, 0);
-        } else {  // CERRADA.
-            gpio_set_level(VALVULA_PIN_1, 0);
-            gpio_set_level(VALVULA_PIN_2, 1);
+        if (valvula != valvula_ant) {
+            if (valvula == ABIERTA) {
+                gpio_set_level(VALVULA_PIN_1, 1);
+                gpio_set_level(VALVULA_PIN_2, 0);
+                printf("### Válvula abierta ###\n");
+            } else {  // CERRADA.
+                gpio_set_level(VALVULA_PIN_1, 0);
+                gpio_set_level(VALVULA_PIN_2, 1);
+                printf("--- Válvula cerrada ---\n");
+            }
+            valvula_ant = valvula;
         }
-        if (bombaN == ABIERTA)
-            gpio_set_level(BOMBA_N_PIN, 1);
-        else  // CERRADA.
-            gpio_set_level(BOMBA_N_PIN, 0);
-        if (bombaP == ABIERTA)
-            gpio_set_level(BOMBA_P_PIN, 1);
-        else  // CERRADA.
-            gpio_set_level(BOMBA_P_PIN, 0);
-        if (bombaK == ABIERTA)
-            gpio_set_level(BOMBA_K_PIN, 1);
-        else  // CERRADA.
-            gpio_set_level(BOMBA_K_PIN, 0);
+        if (bombaN != bombaN_ant) {
+            if (bombaN == ABIERTA) {
+                gpio_set_level(BOMBA_N_PIN, 1);
+                printf("### Bomba N abierta ###\n");
+            } else {  // CERRADA.
+                gpio_set_level(BOMBA_N_PIN, 0);
+                printf("--- Bomba N cerrada ---\n");
+            }
+            bombaN_ant = bombaN;
+        }
+        if (bombaP != bombaP_ant) {
+            if (bombaP == ABIERTA) {
+                gpio_set_level(BOMBA_P_PIN, 1);
+                printf("### Bomba P abierta ###\n");
+            } else {  // CERRADA.
+                gpio_set_level(BOMBA_P_PIN, 0);
+                printf("--- Bomba P cerrada ---\n");
+            }
+            bombaP_ant = bombaP;
+        }
+        if (bombaK != bombaK_ant) {
+            if (bombaK == ABIERTA) {
+                gpio_set_level(BOMBA_K_PIN, 1);
+                printf("### Bomba K abierta ###\n");
+            } else {  // CERRADA.
+                gpio_set_level(BOMBA_K_PIN, 0);
+                printf("--- Bomba K cerrada ---\n");
+            }
+            bombaK_ant = bombaK;
+        }
         
-        vTaskDelay(50);
+        vTaskDelay(40);
     }
 }
 
 void principal(void) {
-    enum estado1 modo_ant = RECIBIR;  // Modo previo del sistema.
-    enum estado1 modo_aux = modo;
+    enum estadoSist modo_ant = RECIBIR;  // Modo previo del sistema.
+    enum estadoSist modo_aux = modo;
     int nutrientes = 0;  // Controla el nutriente que se dispensa: 0=nada, 1=N, 2=P, 3=K.
 
     while (1) {
@@ -171,36 +199,44 @@ void app_main(void) {
     gpio_set_direction(BOMBA_K_PIN, GPIO_MODE_OUTPUT);
     gpio_set_direction(VALVULA_PIN_1, GPIO_MODE_OUTPUT);
     gpio_set_direction(VALVULA_PIN_2, GPIO_MODE_OUTPUT);
-    gpio_set_direction(TEMP_SENSOR_PIN, GPIO_MODE_INPUT);
+    //gpio_set_direction(TEMP_SENSOR_PIN, GPIO_MODE_INPUT);
     gpio_set_direction(NIVEL_SENSOR_PIN, GPIO_MODE_INPUT);
     esp_adc_cal_characterize(ADC_UNIT_1, ADC_ATTEN_DB_11, ADC_WIDTH_BIT_DEFAULT, 0, &adc1_chars);
     ESP_ERROR_CHECK(adc1_config_width(ADC_WIDTH_BIT_DEFAULT));
     ESP_ERROR_CHECK(adc1_config_channel_atten(ELECTRO_SENSOR_PIN, ADC_ATTEN_DB_11));
 
+    ESP_ERROR_CHECK(adc1_config_channel_atten(TEMP_SENSOR_PIN, ADC_ATTEN_DB_11));
+
     ds18b20_init(TEMP_SENSOR_PIN);  // Iniciamos la sonda de temperatura.
-    iniciar_mqtt();  // Iniciamos la conexión MQTT.
+    //iniciar_mqtt();  // Iniciamos la conexión MQTT.
     //iniciar_http();  // Iniciamos la conexión HTTP.
     
-    mqtt_mandar_credenciales_telegram();  // Mandamos las credenciales de Telegram a ThingsBoard como atributos del servidor.
+    //mqtt_mandar_credenciales_telegram();  // Mandamos las credenciales de Telegram a ThingsBoard como atributos del servidor.
 
     // Iniciamos las tareas.
-    xTaskCreate(principal, "Modos sistema", 512, NULL, 9, NULL);
-    xTaskCreate(control_Bombas_Valvula, "Abrir_Cerrar", 512, NULL, 8, NULL);
+    //xTaskCreate(principal, "Modos sistema", 1024, NULL, 9, NULL);
+    xTaskCreate(control_Bombas_Valvula, "Abrir_Cerrar", 1024, NULL, 8, NULL);
 
-    float i = 0.5;
-    tempValor = 19.5;
+    int z = 0;
 
-    while(1){     
-        //medir_temperatura();
+    while (1) {
+        medir_temperatura();
         medir_nivel_tanque();
         medir_electroconductividad();
-        //nivelValor = 0;
-        tempValor += i;
-        mqtt_mandar_datos(tempValor, nivelValor, ecValor);
-        vTaskDelay(1500);
-        if (tempValor > 27.5)
-            i = -0.5;
-        else if (tempValor < 17.5)
-            i = 0.5;
+
+        if (z == 0){
+            bombaN = ABIERTA;
+            bombaP = CERRADA;
+            bombaK = CERRADA;
+            valvula = ABIERTA;
+            z = 1;
+        } else {
+            bombaN = CERRADA;
+            bombaP = ABIERTA;
+            bombaK = ABIERTA;
+            valvula = CERRADA;
+            z = 0;
+        }
+        vTaskDelay(900);
     }
 }
